096750e8e488943af6871b71b163af69b1201f64
Hello-Moeka <39626367+Hello-Moeka@users.noreply.github.com>
2018-06-25 22:16:05 +0800

    Update header.php
    新增文章页显示文章标题、添加og标签

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL | E_STRICT);
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title><?php $this->options->title(); ?></title>
+    <title><?php $this->archiveTitle(array('category'=>_t('分类 %s 下的文章'),'search'=>_t('包含关键字 %s 的文章'),'tag'=>_t('标签 %s 下的文章'),'author'=>_t('%s 发布的文章')),'',' - '); ?><?php $this->options->title(); ?></title>
     <link rel="stylesheet/less" href="<?php $this->options->themeUrl('css/w.less?v2'); ?>">
     <script src="<?php $this->options->themeUrl('js/plugins.js'); ?>" type="text/javascript" charset="utf-8"></script>
     <script src="<?php $this->options->themeUrl('js/m.js?v5'); ?>" type="text/javascript" charset="utf-8"></script>
@@ -42,6 +42,11 @@ error_reporting(E_ALL | E_STRICT);
 	    <?php } ?>
 	</style>
 	<?php $this->header(); ?>
+<meta property="og:url" content="https://Website/">
+<meta property="og:type" content="blog">
+<meta property="og:title" content="Title">
+<meta property="og:description" content="Description">
+<meta property="og:image" content="Image">
 </head>
 <body>
     <?php 
